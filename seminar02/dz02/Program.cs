40e947d0e48518eebc7602514d0a6c3d405e1854
Konstantin Manuylov <76748916+KonstantinManuylov@users.noreply.github.com>
2022-07-08 16:11:54 +0400

    Delete seminar02 directory

--- seminar02/dz02/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-// 456 -> 46. 782 -> 72. 918 -> 98
-
-Random rnd = new Random();
-int number = rnd.Next(100, 1000);
-Console.WriteLine($"Число: {number}");
-string stringNumber = Convert.ToString(number);
-Console.WriteLine($"{stringNumber[0]}{stringNumber[2]}");
