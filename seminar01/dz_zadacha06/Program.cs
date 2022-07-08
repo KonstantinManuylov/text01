d2a1d26f80cc5b9b5813f8b9fb9086ed962a07c0
Konstantin Manuylov <76748916+KonstantinManuylov@users.noreply.github.com>
2022-07-08 16:11:38 +0400

    Delete seminar01 directory

--- seminar01/dz_zadacha06/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Число, ответ, четное или нечетное
-Random rnd = new Random();
-int number = rnd.Next(1, 1001);
-if (number % 2 != 0)
-{
-    Console.WriteLine($"Число {number} нечетное.");
-}
-else Console.WriteLine($"Число {number} четное.");
--- seminar01/zadacha00/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.WriteLine("Введите число ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Ответ: {number * number}");
