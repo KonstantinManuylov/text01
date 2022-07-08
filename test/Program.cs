404f65bf130e0713c2674d5a8bd721680d650f71
KonstantinM <konstnatinmanuijlov@gmail.com>
2022-07-08 16:03:02 +0400

    Added two strings arrays and first method TruePrint

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] words = {  "hello", "2", "world", ":-)", "1234", "1567",
+                    "-2", "computer science", "Russia", "Denmark", "Kazan"};
+string[] finishWords = NewWords(words);
+
+string TruePrint(string[] array)
+{
+    string res = string.Empty;
+    int size = array.Length;
+    res = "[ ";
+    for (int i = 0; i < size; i++)
+    {
+        res += $"{array[i],3} ";
+    }
+    res += "]";
+    return res;
+}
