3b8ff2a95600d339ef40a97627aceeb24587f91e
KonstantinM <konstnatinmanuijlov@gmail.com>
2022-07-08 16:06:14 +0400

    Added method to fill finish array with words with 3 or less chars

--- test/Program.cs
+++ test/Program.cs
@@ -2,6 +2,23 @@
                     "-2", "computer science", "Russia", "Denmark", "Kazan"};
 string[] finishWords = NewWords(words);
 
+string[] NewWords(string[] array)
+{
+    string[] finishArray = new string[Range(array)];
+    int charAmount = 3;
+    int index = 0;
+    string temp = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= charAmount)
+        {
+            finishArray[index] = array[i];
+            index++;
+        }
+    }
+    return finishArray;
+}
+
 int Range(string[] array)
 {
     int charAmount = 3;
