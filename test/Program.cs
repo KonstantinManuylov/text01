46b96b98c51b57c4678a4e6aa6b3eb658b86e886
KonstantinM <konstnatinmanuijlov@gmail.com>
2022-07-08 16:04:14 +0400

    Added method to define the range of final array

--- test/Program.cs
+++ test/Program.cs
@@ -2,6 +2,18 @@
                     "-2", "computer science", "Russia", "Denmark", "Kazan"};
 string[] finishWords = NewWords(words);
 
+int Range(string[] array)
+{
+    int charAmount = 3;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= charAmount)
+            count++;
+    }
+    return count;
+}
+
 string TruePrint(string[] array)
 {
     string res = string.Empty;
